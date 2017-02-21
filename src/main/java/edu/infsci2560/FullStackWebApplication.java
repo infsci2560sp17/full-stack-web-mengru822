740b9245f19b296efe615d98b8cce6813d216967
mengru822 <mengru822@gmail.com>
2017-02-21 05:36:59 +0000

    Assignment1_3Finally

--- src/main/java/edu/infsci2560/FullStackWebApplication.java
+++ src/main/java/edu/infsci2560/FullStackWebApplication.java
@@ -1,15 +1,15 @@
 package edu.infsci2560;
 
-import edu.infsci2560.models.Customer;
-import edu.infsci2560.repositories.CustomerRepository;
+import edu.infsci2560.models.Dance;
+import edu.infsci2560.models.Dance.DanceType;
+import edu.infsci2560.repositories.DanceRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class FullStackWebApplication {
@@ -17,8 +17,15 @@ public class FullStackWebApplication {
     private static final Logger log = LoggerFactory.getLogger(FullStackWebApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(FullStackWebApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(FullStackWebApplication.class, args);
+
+        DanceRepository repository = ctx.getBean(DanceRepository.class);
+        repository.save(new Dance(1L, "Chinese Folk", DanceType.Folk,"easy","14 min"));
+        repository.save(new Dance(2L, "Popping", DanceType.Street,"medium", "20 min"));
+        repository.save(new Dance(3L, "France Ballet", DanceType.Ballet, "hard", "15 min"));
+        repository.save(new Dance(4L, "Samba", DanceType.Ballroom, "easy", "18 min"));
     }
+
 
 //    @Bean
 //    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
@@ -34,37 +41,37 @@ public class FullStackWebApplication {
 //
 //        };
 //    }
-    @Bean
-    public CommandLineRunner databaseDemo(CustomerRepository repository) {
-        return (args) -> {
-            // save a couple of customers
-            repository.save(new Customer("Jack", "Bauer"));
-            repository.save(new Customer("Chloe", "O'Brian"));
-            repository.save(new Customer("Kim", "Bauer"));
-            repository.save(new Customer("David", "Palmer"));
-            repository.save(new Customer("Michelle", "Dessler"));
-            repository.save(new Customer("Billy", "Bean"));
-
-            // fetch all customers
-            log.info("[Database Demo] Customers found with findAll():");
-            log.info("[Database Demo] -------------------------------");
-            for (Customer customer : repository.findAll()) {
-                log.info("[Database Demo] " + customer.toString());
-            }
-            log.info("");
-
-            // fetch an individual customer by ID
-            Customer customer = repository.findOne(1L);
-            log.info("[Database Demo] Customer found with findOne(1L):");
-            log.info("[Database Demo] --------------------------------");
-            log.info("[Database Demo] " + customer.toString());            
-
-            // fetch customers by last name
-            log.info("[Database Demo] Customer found with findByLastName('Bauer'):");
-            log.info("[Database Demo] --------------------------------------------");
-            for (Customer bauer : repository.findByLastName("Bauer")) {
-                log.info("[Database Demo] " + bauer.toString());
-            }            
-        };
-    }
+//    @Bean
+//    public CommandLineRunner databaseDemo(CustomerRepository repository) {
+//        return (args) -> {
+//            // save a couple of customers
+//            repository.save(new Customer("Jack", "Bauer"));
+//            repository.save(new Customer("Chloe", "O'Brian"));
+//            repository.save(new Customer("Kim", "Bauer"));
+//            repository.save(new Customer("David", "Palmer"));
+//            repository.save(new Customer("Michelle", "Dessler"));
+//            repository.save(new Customer("Billy", "Bean"));
+//
+//            // fetch all customers
+//            log.info("[Database Demo] Customers found with findAll():");
+//            log.info("[Database Demo] -------------------------------");
+//            for (Customer customer : repository.findAll()) {
+//                log.info("[Database Demo] " + customer.toString());
+//            }
+//            log.info("");
+//
+//            // fetch an individual customer by ID
+//            Customer customer = repository.findOne(1L);
+//            log.info("[Database Demo] Customer found with findOne(1L):");
+//            log.info("[Database Demo] --------------------------------");
+//            log.info("[Database Demo] " + customer.toString());
+//
+//            // fetch customers by last name
+//            log.info("[Database Demo] Customer found with findByLastName('Bauer'):");
+//            log.info("[Database Demo] --------------------------------------------");
+//            for (Customer bauer : repository.findByLastName("Bauer")) {
+//                log.info("[Database Demo] " + bauer.toString());
+//            }
+//        };
+//    }
 }
--- src/main/java/edu/infsci2560/repositories/DanceRepository.java
+++ src/main/java/edu/infsci2560/repositories/DanceRepository.java
@@ -8,19 +8,4 @@ package edu.infsci2560.repositories;
 import edu.infsci2560.models.Dance;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface DanceRepository extends PagingAndSortingRepository<Dance,Long> {
-
-
-  /*@Override
-  @RestResource(exported = false)
-  void delete(Long long);
-  @Override
-  @RestResource(exported = false)
-  void delete(Dance dance);
-  @Override
-  @RestResource(exported = false)
-  void delete(Iterable<? extends Dance> iterable);
-  @Override
-  @RestResource(exported = false)
-  void deleteAll(); */
-}
+public interface DanceRepository extends PagingAndSortingRepository<Dance,Long> {}
